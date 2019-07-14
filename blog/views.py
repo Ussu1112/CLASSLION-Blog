9465fb8d6e5aedaa05bdfb009371b548eb2b05ee
SeongMin Han <hsm950825@naver.com>
2019-07-14 22:14:13 +0900

    게시글 작성 페이지 추가

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+import datetime
+from django.utils import timezone
 from .models import Blog
 
 def home(request):
-
     blogs = Blog.objects
     return render(request, 'home.html', {'blogs' : blogs})
 
 def detail(request, blog_id):
-
     blog_detail = get_object_or_404(Blog, pk = blog_id) 
     return render(request, 'detail.html', {'blog' : blog_detail})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request): # 입력받은 내용을 데이터베이스에 넣어주는 함수
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    return redirect('/blog/'+str(blog.id))
