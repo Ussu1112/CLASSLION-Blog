d2061e3f677fc908ffdcd5e32d23399081767675
SeongMin Han <hsm950825@naver.com>
2019-07-15 23:35:11 +0900

    템플릿 상속 및 URL 관리

--- secondproject/urls.py
+++ secondproject/urls.py
@@ -25,9 +25,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name="home"),
-    path('blog/<int:blog_id>', blog.views.detail, name="detail"),
-    path('blog/new', blog.views.new, name="new"),
-    path('blog/create', blog.views.create, name='create'),
+    path('blog/', include('blog.urls')),
     path('portfolio/', portfolio.views.portfolio, name="portfolio"),
 ] 
 
